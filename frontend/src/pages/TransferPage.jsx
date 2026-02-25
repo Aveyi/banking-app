75811a79f0950d83cd043f94f8ea149e9896ff8f
Yaroslav <sazanovaroslav4@gmail.com>
2026-02-25 13:26:51 +0300

    Added TransferPage with styles

--- frontend/src/pages/TransferPage.jsx
+++ frontend/src/pages/TransferPage.jsx
@@ -1,7 +1,63 @@
-
+import { useState } from "react";
 
 function TransferPage() {
-  return <h1>Transfer</h1>;
+  const [mode, setMode] = useState("phone");
+  const [recipient, setRecipient] = useState("");
+  const [amount, setAmount] = useState("");
+
+  return (
+    <div className="dashboard">
+      <div className="transfer-container">
+          <div className="widget transfer-widget">
+        <h3>Новый перевод</h3>
+
+        <div className="transfer-switch">
+          <button
+            className={mode === "phone" ? "active" : ""}
+            onClick={() => setMode("phone")}
+          >
+            По телефону
+          </button>
+
+          <button
+            className={mode === "card" ? "active" : ""}
+            onClick={() => setMode("card")}
+          >
+            По карте
+          </button>
+        </div>
+
+        <div className="form-row">
+          <label>
+            {mode === "phone"
+              ? "Номер телефона получателя"
+              : "Номер карты"}
+          </label>
+
+          <input
+            type={mode === "phone" ? "tel" : "text"}
+            value={recipient}
+            onChange={(e) => setRecipient(e.target.value)}
+            placeholder=" "
+          />
+        </div>
+
+        <div className="form-row">
+          <label>Сумма перевода</label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder=" "
+          />
+        </div>
+
+        <button className="login-btn">Перевести</button>
+      </div>
+    </div>
+      </div>
+      
+  );
 }
 
 export default TransferPage;
