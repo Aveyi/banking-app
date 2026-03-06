6259c47aec2b8705650be0d19a5d9bcb31f9178e
Yaroslav <sazanovaroslav4@gmail.com>
2026-03-06 12:18:28 +0300

    Added CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Application definition
 
@@ -42,9 +44,11 @@ INSTALLED_APPS = [
     'transactions',
     'accounts',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
